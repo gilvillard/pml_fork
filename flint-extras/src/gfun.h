d9e72f0c43748de1897b6438f43d568321dc0729
Gilles Villard <gilles.villard@ens-lyon.fr>
2026-05-12 09:42:10 +0200

    resultant and mulmod

--- flint-extras/src/gfun.h
+++ flint-extras/src/gfun.h
@@ -16,7 +16,31 @@
 #include "nmod_poly_mat_extra.h"
 
 
+/** 
+ *    Assume that the degree r in the second variable, say y, is the row dimension - 1 of PT
+ */
+
 void mat_to_xy(nmod_mpoly_t P, nmod_mpoly_ctx_t ctx, const nmod_poly_mat_t PT);
+
+
+/**  Resultant of P and the derivative Py, and the inverse of Py mod P times the resultant 
+ *     as poly_mat
+ *    deg_y P = r, hence PT has r+1 rows 
+ */
+
+void nmod_biv_resultant_geometric(nmod_poly_t Delta, nmod_poly_mat_t  iPyT, const nmod_poly_mat_t PT); 
+
+
+/** Geometric bivariate multiplication A*B mod P, with respect to y 
+ *    the geometric progression is initialized outside
+ * 
+ *   The memainder is known - in advance - to be a polynomial 
+ *    the geometric progression is driven, in particular, by its x-degree 
+ */
+
+void nmod_biv_mulmod_geometric(nmod_poly_mat_t  RT, const nmod_poly_mat_t AT, const nmod_poly_mat_t BT, \
+                                const nmod_poly_mat_t PT,  const ulong D);
+                    
 
 
 #endif // GFUN_H
